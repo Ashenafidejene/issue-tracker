0a781107dcd9a82c5b66fdd86d2676a6f350eba8
Ashenafidejene <ashenafidejene75@gmail.com>
2023-11-15 12:35:01 +0300

    setup prisma

--- issue-tracker/app/Nav.tsx
+++ issue-tracker/app/Nav.tsx
@@ -1,18 +1,28 @@
+'use client';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { FaBug } from "react-icons/fa";
+import classNames from 'classnames';
+import { Console } from 'console';
+
 const Nav = () => {
+   const currentPath = usePathname();
+   //console.log(currentPath)
     const Links=[
         {label:'Dashboard',href:'/'},
-        {label:'Issue',href:'/Issue'},
+        {label:'Issue',href:'/issue'},
     ]
   return (
     
    <nav className='flex space-x-6 border-b-2 mb-3 h-16 items-center text-lg px-12'>
    <Link href='/'><FaBug /></Link>
    <ul className='flex space-x-6 '>
-    {Links.map(links=><Link key={links.href} className='text-zinc-500 hover:text-zinc-800 transition-color ' href={links.href}>{links.label}</Link>)}
-    
+    {Links.map(links=><Link key={links.href} className={classNames({
+        'text-zinc-900':currentPath===links.href,
+        'text-zinc-500':currentPath!==links.href,
+        ' hover:text-zinc-800 transition-color ':true
+    })}  href={links.href}>{links.label}</Link>)} 
    </ul>
    </nav>
   )
---
+++ issue-tracker/app/issue/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IssuePage = () => {
+  return (
+    <div>
+      <h1>this from issue page</h1>
+    </div>
+  )
+}
+
+export default IssuePage
